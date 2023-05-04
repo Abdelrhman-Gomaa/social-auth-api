b10bd6da6e17001146d9968fe27656aab476bbe4
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-04 10:17:26 +0300

    fix: mail to send otp

--- src/_common/mail/mail.processor.ts
+++ src/_common/mail/mail.processor.ts
@@ -9,68 +9,67 @@ import { MailDetails } from './mail.type';
 @Processor('mail-otp')
 @Injectable()
 export class MailProcessor {
-    private from: any;
-    private transporter: any;
+  private from: any;
+  private transporter: any;
 
-    constructor(
-        @InjectQueue('mail-otp') private readonly mailQueue: Queue
-        ) {
-        const fromEnv =
-        'abdelrhman.gomaa@baianat.com:ubtbyzwztkauavzr' || process.env.MAIL_ACCOUNT;
-        const fromDetails = (fromEnv || 'email:password').split(':');
-    
-        this.from = {
-          mail: fromDetails[0],
-          password: fromDetails[1]
-        };
-    
-        this.transporter = nodemailer.createTransport({
-          service: 'gmail',
-          host: 'smtp.gmail.com',
-          secureConnection: false,
-          secure: false,
-          port: 587,
-          auth: {
-            user: this.from.mail,
-            pass: this.from.password
-          }
-        });
+  constructor(
+    @InjectQueue('mail-otp') private readonly mailQueue: Queue
+  ) {
+    const fromEnv = process.env.MAIL_ACCOUNT;
+    const fromDetails = (fromEnv || 'email:password').split(':');
+
+    this.from = {
+      mail: fromDetails[0],
+      password: fromDetails[1]
+    };
+
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      secureConnection: false,
+      secure: false,
+      port: 587,
+      auth: {
+        user: this.from.mail,
+        pass: this.from.password
       }
+    });
+  }
 
-    public async send(input: MailDetails) {
-        const { to, html, from, subject } = input;
-        const transporter = this.transporter;
-        try {
-          await transporter.sendMail({
-            from: from || this.from.mail,
-            to,
-            subject,
-            html
-          });
-        } catch (e) {
-          console.log(e);
-        }
-      }
-
-    @Process({
-        name: 'mail-otp',
-        concurrency: os.cpus().length
-    })
-    async sendEmail(job: Job) {
-        const input : MailDetails = job.data
-        if (!input.from) {
-            input.from = process.env.MAIL_NAME;
-        }
-        if (!input.text) {
-            input.text = process.env.DEFAULT_SUBJECT;
-        }
-        await this.transporter.sendMail(job.data);
+  public async send(input: MailDetails) {
+    const { to, html, from, subject } = input;
+    const transporter = this.transporter;
+    try {
+      await transporter.sendMail({
+        from: from || this.from.mail,
+        to,
+        subject,
+        html
+      });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    @OnQueueActive()
-    onActive(job: Job) {
-        console.log(
-            `Processing job ${job.id} of type ${job.name} `,
-        );
+  @Process({
+    name: 'mail-otp',
+    concurrency: os.cpus().length
+  })
+  async sendEmail(job: Job) {
+    const input: MailDetails = job.data;
+    if (!input.from) {
+      input.from = process.env.MAIL_NAME;
     }
+    if (!input.text) {
+      input.text = process.env.DEFAULT_SUBJECT;
+    }
+    await this.transporter.sendMail(job.data);
+  }
+
+  @OnQueueActive()
+  onActive(job: Job) {
+    console.log(
+      `Processing job ${job.id} of type ${job.name} `,
+    );
+  }
 }
